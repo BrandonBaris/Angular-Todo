71ea75053c4c2e8ef9c1cb7bc15fabe2fc50bee0
brandonbaris <brandonbaris@gmail.com>
2015-03-09 12:01:50 -1000

    adds express server

---
+++ server/controllers/api.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+//listalltodos
+router.get('/', function (req,res){
+  res.send("HELLO API");
+});
+
+//add todo
+
+//delete todo 
+
+//complete todo
+
+//incomplete todo
+
+module.exports = router;
