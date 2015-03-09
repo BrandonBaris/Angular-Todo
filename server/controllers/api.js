39e6360d1ec8b69b211151f8388c12e2c1f2f3f5
brandonbaris <brandonbaris@gmail.com>
2015-03-09 12:38:32 -1000

    adds routes

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -1,17 +1,57 @@
 var express = require('express');
 var router = express.Router();
+var Todo = require('../models/todo');
 
-//listalltodos
-router.get('/', function (req,res){
-  res.send("HELLO API");
+//list all todos
+router.get('/', function ( req,res ){
+  Todo.find(function ( err,todos ){
+    if (err) throw err;
+    res.json( todos );
+  });
 });
 
 //add todo
+router.post('/', function (req,res){
+
+  Todo.create({ title: req.body.title }, function(err, todo) {
+    if(err) throw err;
+    res.json( todo );
+  });
+});
 
 //delete todo 
+router.delete('/:id', function (req,res){
+  Todo.remove( req.params.id , function (err, todo){
+    if(err) throw err;
+    res.json( todo ); // this is the todo that was removed
+  });
+});
 
 //complete todo
+router.put('/:id/complete', function (req,res){
+  console.log(req.params.id);
+  Todo.update( req.params.id , 
+  {
+    $set : {
+      completed : true
+    }
+  }, function (err, todo){
+    if(err) throw err;
+    res.json( todo ); // this is the todo that was updated
+  });
+});
 
 //incomplete todo
+router.put('/:id/incomplete', function (req,res){
+  Todo.update( req.params.id , 
+  {
+    $set : {
+      completed : false
+    }
+  }, function (err, todo){
+    if(err) throw err;
+    res.json( todo ); // this is the todo that was updated
+  });
+});
 
 module.exports = router;
